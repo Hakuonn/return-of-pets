2228419216e2aececb770c682a8010daff025079
趙伯恩 <hakuonn@hakuonn-MacBook-Air.local>
2023-05-31 14:34:50 +0800

    0531

---
+++ js/showPage/feedback.js
@@ -0,0 +1,8 @@
+export default function showFeedbackPage(){
+    let feedbackPage = `
+    document.getElementById('root').innerHTML = feedbackPage
+    document.getElementById('send_message').onclick = function(){
+        doSendMessage();
+    `
+}
+
