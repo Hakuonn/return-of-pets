fa560464b6323258127735d9d421547691b012ed
趙伯恩 <hakuonn@hakuonn-MacBook-Air.local>
2023-06-07 01:34:38 +0800

    0607

---
+++ js/formFunc/login.js
@@ -0,0 +1,7 @@
+//GET請求
+import { doc } from "prettier";
+import request from "../request.js";
+
+function getLogin() {
+    
+}
--- js/showPage/feedback.js
+++
@@ -1,8 +0,0 @@
-export default function showFeedbackPage(){
-    let feedbackPage = `
-    document.getElementById('root').innerHTML = feedbackPage
-    document.getElementById('send_message').onclick = function(){
-        doSendMessage();
-    `
-}
-
